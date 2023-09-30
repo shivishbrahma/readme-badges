e1c01aa79742a9d30c54adb5a0d5ccf572c55af1
Purbayan Chowdhury <pur.cho.99@gmail.com>
2023-09-30 17:58:01 +0530

    Querying graphql for repo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const github = require('@actions/github')
 
 /**
  * The main function for the action.
@@ -7,15 +7,39 @@ const { wait } = require('./wait')
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+    const token = core.getInput('GH_TOKEN', { required: true }),
+      owner = github.context.repo.owner,
+      repo = github.context.repo.repo
+    console.log(`GH_TOKEN ${token}!`)
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const octokit = github.getOctokit(token)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const headers = {
+      'User-Agent': 'Web/2.0',
+      Authorization: 'Bearer ' + token
+    }
+
+    const graphQLWithAuth = octokit.graphql.defaults({ headers })
+
+    const { repository } = await graphQLWithAuth(`{
+        repository(name: "${repo}", owner: "${owner}") {
+            name
+            nameWithOwner
+            description
+            licenseInfo {name}
+            languages (first: 5, orderBy: {direction: DESC,field: SIZE}){
+                edges {
+                    node {
+                        name
+                        color
+                    }
+                    size
+                }
+            }
+        }
+    }`)
+
+    console.log(`Repository: ${JSON.stringify(repository, undefined, 2)}`)
 
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
