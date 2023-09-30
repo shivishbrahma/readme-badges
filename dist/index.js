533b49f4fd4a900d5b3f6a809560deb2056cb0c0
Purbayan Chowdhury <pur.cho.99@gmail.com>
2023-09-30 22:00:17 +0530

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -9636,6 +9636,7 @@ function wrappy (fn, cb) {
 
 const core = __nccwpck_require__(2186)
 const github = __nccwpck_require__(5438)
+const { generateLicenseBadge } = __nccwpck_require__(1608)
 
 /**
  * The main function for the action.
@@ -9643,10 +9644,13 @@ const github = __nccwpck_require__(5438)
  */
 async function run() {
   try {
+    // Constant for GitHub Actions
+    const showLicense = core.getBooleanInput('SHOW_LICENSE')
+    const badgeStyle = core.getInput('BADGE_STYLE')
     const token = core.getInput('GH_TOKEN', { required: true })
+
     const owner = github.context.repo.owner
     const repo = github.context.repo.repo
-    console.log(`GH_TOKEN ${token}!`)
 
     const octokit = github.getOctokit(token)
 
@@ -9657,12 +9661,14 @@ async function run() {
 
     const graphQLWithAuth = octokit.graphql.defaults({ headers })
 
+    const badges = []
+
     const { repository } = await graphQLWithAuth(`{
         repository(name: "${repo}", owner: "${owner}") {
             name
             nameWithOwner
             description
-            licenseInfo {name}
+            licenseInfo {name, spdxId}
             languages (first: 5, orderBy: {direction: DESC,field: SIZE}){
                 edges {
                     node {
@@ -9675,7 +9681,12 @@ async function run() {
         }
     }`)
 
-    console.log(`Repository: ${JSON.stringify(repository, undefined, 2)}`)
+    // Generate License Badge
+    if (!!repository && showLicense) {
+      badges.push(generateLicenseBadge(repository.licenseInfo, badgeStyle))
+    }
+
+    console.log(JSON.stringify(badges, undefined, 2))
 
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
@@ -9687,6 +9698,156 @@ async function run() {
 
 module.exports = {
   run
+}
+
+
+/***/ }),
+
+/***/ 1608:
+/***/ ((module) => {
+
+function createBadgeUrl(
+  title,
+  desc,
+  descColor,
+  style = 'flat', // flat, flat-square, plastic, for-the-badge, social
+  logo = null,
+  logoColor = null,
+  label = null,
+  labelColor = null,
+  color = null
+) {
+  return `https://img.shields.io/badge/${encodeURI(title)}-${encodeURI(
+    desc
+  )}-${encodeURI(descColor)}?style=${encodeURI(style)}${
+    !!logo ? '&logo=' + encodeURI(logo) : ''
+  }${!!logoColor ? '&logoColor=' + encodeURI(logoColor) : ''}${
+    !!label ? '&label=' + encodeURI(label) : ''
+  }${!!labelColor ? '&labelColor=' + encodeURI(labelColor) : ''}${
+    !!color ? '&color=' + encodeURI(color) : ''
+  }`
+}
+
+const licenseTypes = {
+  // permissive licenses - not public domain and not copyleft
+  permissive: {
+    spdxLicenseIds: [
+      'AFL-3.0',
+      'Apache-2.0',
+      'Artistic-2.0',
+      'BSD-2-Clause',
+      'BSD-3-Clause',
+      'BSD-3-Clause-Clear',
+      'BSL-1.0',
+      'CC-BY-4.0',
+      'ECL-2.0',
+      'ISC',
+      'MIT',
+      'MS-PL',
+      'NCSA',
+      'PostgreSQL',
+      'Zlib'
+    ],
+    aliases: ['BSD', 'Apache 2.0'],
+    color: 'green',
+    priority: '2'
+  },
+  // copyleft licenses require 'Disclose source' (https://choosealicense.com/appendix/#disclose-source)
+  // or 'Same license' (https://choosealicense.com/appendix/#same-license)
+  copyleft: {
+    spdxLicenseIds: [
+      'AGPL-1.0-only',
+      'AGPL-1.0-or-later',
+      'AGPL-3.0-only',
+      'AGPL-3.0-or-later',
+      'CC-BY-SA-4.0',
+      'EPL-1.0',
+      'EPL-2.0',
+      'EUPL-1.1',
+      'GPL-1.0-only',
+      'GPL-1.0-or-later',
+      'GPL-2.0-only',
+      'GPL-2.0-or-later',
+      'GPL-3.0-only',
+      'GPL-3.0-or-later',
+      'LGPL-2.0-only',
+      'LGPL-2.0-or-later',
+      'LGPL-2.1-only',
+      'LGPL-2.1-or-later',
+      'LGPL-3.0-only',
+      'LGPL-3.0-or-later',
+      'LPPL-1.3c',
+      'MPL-2.0',
+      'MS-RL',
+      'OFL-1.1',
+      'OSL-3.0'
+    ],
+    aliases: [
+      'GPL',
+      'GPL-2.0',
+      'GPL-3.0',
+      'GPLv2',
+      'GPLv2+',
+      'GPLv3',
+      'GPLv3+',
+      'LGPL',
+      'LGPL-2.1',
+      'LGPL-3.0',
+      'LGPLv2',
+      'LGPLv2+',
+      'LGPLv3',
+      'LGPLv3+',
+      'AGPL-3.0',
+      'AGPLv3+',
+      'MPL',
+      'MPL 1.1',
+      'MPL 2.0',
+      'EPL'
+    ],
+    color: 'orange',
+    priority: '1'
+  },
+  // public domain licenses do not require 'License and copyright notice' (https://choosealicense.com/appendix/#include-copyright)
+  'public-domain': {
+    spdxLicenseIds: ['CC0-1.0', 'Unlicense', 'WTFPL'],
+    aliases: ['CC0'],
+    color: '7cd958',
+    priority: '3'
+  }
+}
+
+/**
+ * Mapping of licenses to their corresponding color and priority.
+ *
+ * @type {object}
+ */
+const licenseToColorMap = {}
+Object.keys(licenseTypes).forEach(licenseType => {
+  const { spdxLicenseIds, aliases, color, priority } = licenseTypes[licenseType]
+  spdxLicenseIds.forEach(license => {
+    licenseToColorMap[license] = { color, priority }
+  })
+  aliases.forEach(license => {
+    licenseToColorMap[license] = { color, priority }
+  })
+})
+
+function generateLicenseBadge(licenseInfo, style) {
+  const title = 'license'
+  let desc = 'not specified'
+  let descColor = 'lightgrey'
+
+  if (!!licenseInfo) {
+    desc = licenseInfo.spdxId
+    descColor = licenseToColorMap[licenseInfo.spdxId]
+  }
+
+  return createBadgeUrl(title, desc, descColor, style)
+}
+
+module.exports = {
+  createBadgeUrl,
+  generateLicenseBadge
 }
 
 
